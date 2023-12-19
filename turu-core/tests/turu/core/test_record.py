06f02ea6c8a5981402c5ba62d07fdb105f38c54b
yassun7010 <yassun7010@outlook.com>
2023-12-20 00:05:36 +0900

    refactor: test case.

--- turu-core/tests/turu/core/test_record.py
+++ turu-core/tests/turu/core/test_record.py
@@ -7,33 +7,34 @@ from tests.data import TEST_DATA_DIR
 
 class RowPydantic(BaseModel):
     id: int
+    name: str
 
 
 class TestRecord:
     def test_record_as_csv_execute(
         self, mock_connection: turu.core.mock.MockConnection
     ):
-        expected = [(i,) for i in range(5)]
+        expected = [(i, f"name{i}") for i in range(5)]
         mock_connection.inject_response(None, expected)
 
         with record_as_csv(
             TEST_DATA_DIR / "test_record_as_csv_execute.csv",
             mock_connection.cursor(),
         ) as cursor:
-            cursor = cursor.execute("select 1")
+            cursor = cursor.execute("select 1, 'taro")
 
             assert cursor.fetchall() == expected
 
     def test_record_as_csv_execute_map(
         self, mock_connection: turu.core.mock.MockConnection
     ):
-        expected = [RowPydantic(id=i) for i in range(5)]
+        expected = [RowPydantic(id=i, name=f"name{i}") for i in range(5)]
         mock_connection.inject_response(RowPydantic, expected)
 
         with record_as_csv(
             TEST_DATA_DIR / "test_record_as_csv_execute_map.csv",
             mock_connection.cursor(),
         ) as cursor:
-            cursor = cursor.execute_map(RowPydantic, "select 1")
+            cursor = cursor.execute_map(RowPydantic, "select 1, 'name")
 
             assert cursor.fetchall() == expected
