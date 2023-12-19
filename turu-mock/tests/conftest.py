43350b7359de6cf06d0857acadcdf214c73ddec7
yassun7010 <yassun7010@outlook.com>
2023-12-19 21:42:08 +0900

    chore: update type hint.

--- turu-mock/tests/conftest.py
+++ turu-mock/tests/conftest.py
@@ -1,9 +1,12 @@
+from typing import Any
+
 import pytest
 import turu.mock
+from typing_extensions import Never
 
 
 class MockConnection(turu.mock.MockConnection):
-    def cursor(self) -> turu.mock.MockCursor:
+    def cursor(self) -> turu.mock.MockCursor[Never, Any]:
         return turu.mock.MockCursor(self._turu_mock_store)
 
 
