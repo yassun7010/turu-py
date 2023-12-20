83e7e81dd894acfd1686ce435cbc9034a88ccef9
yassun7010 <yassun7010@outlook.com>
2023-12-20 11:15:22 +0900

    doc: update comment and remove commentout.

--- turu-core/src/turu/core/connection.py
+++ turu-core/src/turu/core/connection.py
@@ -1,6 +1,1 @@
-# from typingsql.protocols.connection import ConnectionProtocol
-
-
-# class Connection(ConnectionProtocol):
-#     def __init__(self, connection: ConnectionProtocol):
-#         self._connection = connection
+# NOTE: Use ConnectionProtocol
