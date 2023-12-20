5546008c0a0d9fb947a293851457eaa92e229785
yassun7010 <yassun7010@outlook.com>
2023-12-20 23:34:40 +0900

    refactor: test code.

--- turu-snowflake/tests/conftest.py
+++ turu-snowflake/tests/conftest.py
@@ -6,22 +6,14 @@ import turu.snowflake
 
 @pytest.fixture
 def connection() -> turu.snowflake.Connection:
-    SNOWFLAKE_USER = os.environ["SNOWFLAKE_USER"]
-    SNOWFLAKE_PASSWORD = os.environ["SNOWFLAKE_PASSWORD"]
-    SNOWFLAKE_ACCOUNT = os.environ["SNOWFLAKE_ACCOUNT"]
-    SNOWFLAKE_DATABASE = os.environ["SNOWFLAKE_DATABASE"]
-    SNOWFLAKE_SCHEMA = os.environ["SNOWFLAKE_SCHEMA"]
-    SNOWFLAKE_WAREHOUSE = os.environ["SNOWFLAKE_WAREHOUSE"]
-    SNOWFLAKE_ROLE = os.environ["SNOWFLAKE_ROLE"]
-
     return turu.snowflake.connect(
-        user=SNOWFLAKE_USER,
-        password=SNOWFLAKE_PASSWORD,
-        account=SNOWFLAKE_ACCOUNT,
-        database=SNOWFLAKE_DATABASE,
-        schema=SNOWFLAKE_SCHEMA,
-        warehouse=SNOWFLAKE_WAREHOUSE,
-        role=SNOWFLAKE_ROLE,
+        user=os.environ["SNOWFLAKE_USER"],
+        password=os.environ["SNOWFLAKE_PASSWORD"],
+        account=os.environ["SNOWFLAKE_ACCOUNT"],
+        database=os.environ["SNOWFLAKE_DATABASE"],
+        schema=os.environ["SNOWFLAKE_SCHEMA"],
+        warehouse=os.environ["SNOWFLAKE_WAREHOUSE"],
+        role=os.environ["SNOWFLAKE_ROLE"],
     )
 
 
