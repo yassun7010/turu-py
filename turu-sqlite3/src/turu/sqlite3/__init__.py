40bffffdcf858a02c4f728882975381930584cba
yassun7010 <yassun7010@outlook.com>
2023-12-20 12:47:18 +0900

    fix: old code.

--- turu-sqlite3/src/turu/sqlite3/__init__.py
+++ turu-sqlite3/src/turu/sqlite3/__init__.py
@@ -1,5 +1,4 @@
 import importlib.metadata
-from importlib.util import find_spec
 
 from .connection import Connection, connect
 from .cursor import Cursor
@@ -7,20 +6,13 @@ from .cursor import Cursor
 __version__ = importlib.metadata.version("turu-sqlite3")
 
 
-if find_spec("turu.core.mock"):
-    from .connection import MockConnection
-    from .cursor import MockCursor
+from .connection import MockConnection
+from .cursor import MockCursor
 
-    __all__ = [
-        "connect",
-        "Connection",
-        "Cursor",
-        "MockConnection",
-        "MockCursor",
-    ]
-else:
-    __all__ = [
-        "connect",
-        "Connection",
-        "Cursor",
-    ]
+__all__ = [
+    "connect",
+    "Connection",
+    "Cursor",
+    "MockConnection",
+    "MockCursor",
+]
