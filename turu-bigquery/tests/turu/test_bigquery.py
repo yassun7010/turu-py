4d98107bd2cc122d8126b125e5460a5704bc65e1
yassun7010 <yassun7010@outlook.com>
2023-12-20 23:58:43 +0900

    test: add test case.

--- turu-bigquery/tests/turu/test_bigquery.py
+++ turu-bigquery/tests/turu/test_bigquery.py
@@ -2,10 +2,15 @@ import os
 
 import pytest
 import turu.bigquery
+from pydantic import BaseModel
 
 
 def test_version():
     assert turu.bigquery.__version__
+
+
+class PydanticRow(BaseModel):
+    id: int
 
 
 @pytest.mark.skipif(
@@ -27,3 +32,29 @@ class TestBigquery:
             (1,),
             (2,),
         ]
+
+    # def test_executemany(self, connection: turu.bigquery.Connection):
+    #     assert connection.executemany("select 1 union all select 2", []).fetchall() == [
+    #         (1,),
+    #         (2,),
+    #     ]
+
+    def test_execute_map_fetchone(self, connection: turu.bigquery.Connection):
+        with connection.execute_map(PydanticRow, "select 1") as cursor:
+            assert cursor.fetchone() == PydanticRow(id=1)
+            assert cursor.fetchone() is None
+
+    def test_execute_map_fetchmany(self, connection: turu.bigquery.Connection):
+        with connection.execute_map(
+            PydanticRow, "select 1 union all select 2"
+        ) as cursor:
+            assert cursor.fetchmany() == [PydanticRow(id=1)]
+            assert cursor.fetchone() == PydanticRow(id=2)
+            assert cursor.fetchone() is None
+
+    def test_execute_map_fetchall(self, connection: turu.bigquery.Connection):
+        with connection.execute_map(
+            PydanticRow, "select 1 union all select 2"
+        ) as cursor:
+            assert cursor.fetchall() == [PydanticRow(id=1), PydanticRow(id=2)]
+            assert cursor.fetchone() is None
