707bf220b55ab7ba72e4b6746832753c403a7ddb
yassun7010 <yassun7010@outlook.com>
2024-01-21 17:59:57 +0900

    refactor: test cases.

--- turu-core/tests/turu/core/test_record.py
+++ turu-core/tests/turu/core/test_record.py
@@ -1,3 +1,5 @@
+import tempfile
+from pathlib import Path
 from textwrap import dedent
 from typing import Any, Literal
 
@@ -7,8 +9,6 @@ from pydantic import BaseModel
 from turu.core.exception import TuruRowTypeNotSupportedError
 from turu.core.record import _RecordCursor, record_as_csv
 from typing_extensions import Never
-
-from tests.data.record import TEST_RECORD_DIR
 
 
 class RowPydantic(BaseModel):
@@ -23,16 +23,15 @@ class TestRecord:
         expected = [(i, f"name{i}") for i in range(5)]
         mock_connection.inject_response(None, expected)
 
-        with pytest.raises(TuruRowTypeNotSupportedError):
-            with record_as_csv(
-                TEST_RECORD_DIR / "test_record_as_csv_execute_tuple.csv",
-                mock_connection.execute("select 1 as ID, 'taro' as NAME"),
-            ) as cursor:
-                assert cursor.fetchall() == expected
+        with tempfile.NamedTemporaryFile() as file:
+            with pytest.raises(TuruRowTypeNotSupportedError):
+                with record_as_csv(
+                    file.name,
+                    mock_connection.execute("select 1 as ID, 'taro' as NAME"),
+                ) as cursor:
+                    assert cursor.fetchall() == expected
 
-        assert (
-            TEST_RECORD_DIR / "test_record_as_csv_execute_tuple.csv"
-        ).read_text() == dedent("")
+            assert Path(file.name).read_text() == ""
 
     def test_record_as_csv_execute_tuple_without_header(
         self, mock_connection: turu.core.mock.MockConnection
@@ -40,12 +39,26 @@ class TestRecord:
         expected = [(i, f"name{i}") for i in range(5)]
         mock_connection.inject_response(None, expected)
 
-        with record_as_csv(
-            TEST_RECORD_DIR / "test_record_as_csv_execute_tuple_without_header.csv",
-            mock_connection.execute("select 1, 'taro"),
-            header=False,
-        ) as cursor:
-            assert cursor.fetchall() == expected
+        with tempfile.NamedTemporaryFile() as file:
+            with record_as_csv(
+                file.name,
+                mock_connection.execute("select 1, 'taro"),
+                header=False,
+            ) as cursor:
+                assert cursor.fetchall() == expected
+
+            assert (
+                Path(file.name).read_text()
+                == dedent(
+                    """
+                    0,name0
+                    1,name1
+                    2,name2
+                    3,name3
+                    4,name4
+                    """
+                ).lstrip()
+            )
 
     def test_record_as_csv_execute_map(
         self, mock_connection: turu.core.mock.MockConnection
@@ -53,11 +66,26 @@ class TestRecord:
         expected = [RowPydantic(id=i, name=f"name{i}") for i in range(5)]
         mock_connection.inject_response(RowPydantic, expected)
 
-        with record_as_csv(
-            TEST_RECORD_DIR / "test_record_as_csv_execute_map.csv",
-            mock_connection.execute_map(RowPydantic, "select 1, 'name"),
-        ) as cursor:
-            assert cursor.fetchall() == expected
+        with tempfile.NamedTemporaryFile() as file:
+            with record_as_csv(
+                file.name,
+                mock_connection.execute_map(RowPydantic, "select 1, 'name"),
+            ) as cursor:
+                assert cursor.fetchall() == expected
+
+            assert (
+                Path(file.name).read_text()
+                == dedent(
+                    """
+                    id,name
+                    0,name0
+                    1,name1
+                    2,name2
+                    3,name3
+                    4,name4
+                    """
+                ).lstrip()
+            )
 
     def test_record_as_csv_execute_map_without_header_options(
         self, mock_connection: turu.core.mock.MockConnection
@@ -65,13 +93,26 @@ class TestRecord:
         expected = [RowPydantic(id=i, name=f"name{i}") for i in range(5)]
         mock_connection.inject_response(RowPydantic, expected)
 
-        with record_as_csv(
-            TEST_RECORD_DIR
-            / "test_record_as_csv_execute_map_without_header_options.csv",
-            mock_connection.execute_map(RowPydantic, "select 1, 'name"),
-            header=False,
-        ) as cursor:
-            assert cursor.fetchall() == expected
+        with tempfile.NamedTemporaryFile() as file:
+            with record_as_csv(
+                file.name,
+                mock_connection.execute_map(RowPydantic, "select 1, 'name"),
+                header=False,
+            ) as cursor:
+                assert cursor.fetchall() == expected
+
+            assert (
+                Path(file.name).read_text()
+                == dedent(
+                    """
+                    0,name0
+                    1,name1
+                    2,name2
+                    3,name3
+                    4,name4
+                    """
+                ).lstrip()
+            )
 
     def test_record_as_csv_execute_map_with_limit_options(
         self, mock_connection: turu.core.mock.MockConnection
@@ -79,12 +120,25 @@ class TestRecord:
         expected = [RowPydantic(id=i, name=f"name{i}") for i in range(5)]
         mock_connection.inject_response(RowPydantic, expected)
 
-        with record_as_csv(
-            TEST_RECORD_DIR / "test_record_as_csv_execute_map_with_limit_options.csv",
-            mock_connection.execute_map(RowPydantic, "select 1, 'name"),
-            limit=3,
-        ) as cursor:
-            assert cursor.fetchall() == expected
+        with tempfile.NamedTemporaryFile() as file:
+            with record_as_csv(
+                file.name,
+                mock_connection.execute_map(RowPydantic, "select 1, 'name"),
+                limit=3,
+            ) as cursor:
+                assert cursor.fetchall() == expected
+
+            assert (
+                Path(file.name).read_text()
+                == dedent(
+                    """
+                    id,name
+                    0,name0
+                    1,name1
+                    2,name2
+                    """
+                ).lstrip()
+            )
 
     @pytest.mark.parametrize("enable", ["true", True])
     def test_record_as_csv_execute_map_with_enable_options(
@@ -95,26 +149,28 @@ class TestRecord:
         expected = [RowPydantic(id=i, name=f"name{i}") for i in range(5)]
         mock_connection.inject_response(RowPydantic, expected)
 
-        with record_as_csv(
-            TEST_RECORD_DIR / "test_record_as_csv_execute_map_with_enable_options.csv",
-            mock_connection.execute_map(RowPydantic, "select 1, 'name"),
-            enable=enable,
-        ) as cursor:
-            assert isinstance(cursor, _RecordCursor)
-            assert cursor.fetchall() == expected
+        with tempfile.NamedTemporaryFile() as file:
+            with record_as_csv(
+                file.name,
+                mock_connection.execute_map(RowPydantic, "select 1, 'name"),
+                enable=enable,
+            ) as cursor:
+                assert isinstance(cursor, _RecordCursor)
+                assert cursor.fetchall() == expected
 
-        assert (
-            TEST_RECORD_DIR / "test_record_as_csv_execute_map_with_enable_options.csv"
-        ).read_text().strip() == dedent(
-            """
-            id,name
-            0,name0
-            1,name1
-            2,name2
-            3,name3
-            4,name4
-            """
-        ).strip()
+            assert (
+                Path(file.name).read_text()
+                == dedent(
+                    """
+                    id,name
+                    0,name0
+                    1,name1
+                    2,name2
+                    3,name3
+                    4,name4
+                    """
+                ).lstrip()
+            )
 
     @pytest.mark.parametrize("enable", ["false", False, None])
     def test_record_as_csv_execute_map_with_disable_options(
@@ -125,12 +181,15 @@ class TestRecord:
         expected = [RowPydantic(id=i, name=f"name{i}") for i in range(5)]
         mock_connection.inject_response(RowPydantic, expected)
 
-        with record_as_csv(
-            TEST_RECORD_DIR / "test_record_as_csv_execute_map_with_disable_options.csv",
-            mock_connection.execute_map(RowPydantic, "select 1, 'name"),
-            enable=enable,
-        ) as cursor:
-            assert not isinstance(cursor, _RecordCursor)
+        with tempfile.NamedTemporaryFile() as file:
+            with record_as_csv(
+                file.name,
+                mock_connection.execute_map(RowPydantic, "select 1, 'name"),
+                enable=enable,
+            ) as cursor:
+                assert not isinstance(cursor, _RecordCursor)
+
+            assert Path(file.name).read_text() == ""
 
     def test_record_as_csv_use_custom_method(self):
         class CustomCursor(turu.core.mock.MockCursor[Never, Any]):
@@ -144,15 +203,11 @@ class TestRecord:
             def custom_method(self, value: int) -> None:
                 pass
 
-        with record_as_csv(
-            TEST_RECORD_DIR / "test_record_as_csv_use_custom_method.csv",
-            CustomConnection().cursor(),
-        ) as cursor:
-            assert cursor.custom_method(1) is None
+        with tempfile.NamedTemporaryFile() as file:
+            with record_as_csv(
+                file.name,
+                CustomConnection().cursor(),
+            ) as cursor:
+                assert cursor.custom_method(1) is None
 
-        assert (
-            TEST_RECORD_DIR.joinpath(
-                "test_record_as_csv_use_custom_method.csv"
-            ).read_text()
-            == ""
-        )
+            assert Path(file.name).read_text() == ""
