3440a18463e7ea4f54ec8b461d2e91cdbff7b547
yassun7010 <yassun7010@outlook.com>
2024-02-17 00:32:24 +0900

    feat: add use_* methods.

--- turu-snowflake/src/turu/snowflake/record/record_cursor.py
+++ turu-snowflake/src/turu/snowflake/record/record_cursor.py
@@ -7,6 +7,7 @@ from turu.snowflake.features import (
     GenericPandasDataFrame,
     GenericPyArrowTable,
 )
+from typing_extensions import Self
 
 
 class RecordCursor(  # type: ignore[override]
@@ -14,7 +15,7 @@ class RecordCursor(  # type: ignore[override]
     Generic[GenericRowType, GenericPandasDataFrame, GenericPyArrowTable],
 ):
     def fetch_pandas_all(self, **kwargs) -> GenericPandasDataFrame:
-        df = cast(GenericPandasDataFrame, self._cursor.fetch_pandas_all(**kwargs))  # type: ignore[assignment]
+        df = self._sf_cursor.fetch_pandas_all(**kwargs)
 
         if isinstance(self._recorder, turu.core.record.CsvRecorder):
             if limit := self._recorder._options.get("limit"):
@@ -29,10 +30,8 @@ class RecordCursor(  # type: ignore[override]
         return df
 
     def fetch_pandas_batches(self, **kwargs) -> Iterator[GenericPandasDataFrame]:
-        batches = cast(
-            Iterator[GenericPandasDataFrame],
-            self._cursor.fetch_pandas_batches(**kwargs),  # type: ignore[assignment]
-        )
+        batches = self._sf_cursor.fetch_pandas_batches(**kwargs)
+
         if isinstance(self._recorder, turu.core.record.CsvRecorder):
             if limit := self._recorder._options.get("limit"):
                 for batch in batches:
@@ -45,7 +44,7 @@ class RecordCursor(  # type: ignore[override]
         return batches
 
     def fetch_arrow_all(self) -> GenericPyArrowTable:
-        table = cast(GenericPyArrowTable, self._cursor.fetch_arrow_all())  # type: ignore[assignment]
+        table = self._sf_cursor.fetch_arrow_all()
 
         if isinstance(self._recorder, turu.core.record.CsvRecorder):
             if limit := self._recorder._options.get("limit"):
@@ -60,10 +59,8 @@ class RecordCursor(  # type: ignore[override]
         return table
 
     def fetch_arrow_batches(self) -> Iterator[GenericPyArrowTable]:
-        batches = cast(
-            Iterator[GenericPyArrowTable],
-            self._cursor.fetch_arrow_batches(),  # type: ignore[assignment]
-        )
+        batches = self._sf_cursor.fetch_arrow_batches()
+
         if isinstance(self._recorder, turu.core.record.CsvRecorder):
             if limit := self._recorder._options.get("limit"):
                 for batch in batches:
@@ -74,3 +71,39 @@ class RecordCursor(  # type: ignore[override]
                         return
 
         return batches
+
+    def use_warehouse(self, warehouse: str, /) -> Self:
+        """Use a warehouse in cursor."""
+
+        self._sf_cursor.use_warehouse(warehouse)
+
+        return self
+
+    def use_database(self, database: str, /) -> Self:
+        """Use a database in cursor."""
+
+        self._sf_cursor.use_database(database)
+
+        return self
+
+    def use_schema(self, schema: str, /) -> Self:
+        """Use a schema in cursor."""
+
+        self._sf_cursor.use_schema(schema)
+
+        return self
+
+    def use_role(self, role: str, /) -> Self:
+        """Use a role in cursor."""
+
+        self._sf_cursor.use_role(role)
+
+        return self
+
+    @property
+    def _sf_cursor(
+        self,
+    ) -> turu.snowflake.cursor.Cursor[
+        GenericRowType, GenericPandasDataFrame, GenericPyArrowTable
+    ]:
+        return cast(turu.snowflake.cursor.Cursor, self._cursor)
