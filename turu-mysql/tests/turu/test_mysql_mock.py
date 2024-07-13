36b95bd722dbf552de128ff0816ec6a740f4e19e
yassun7010 <yassun7010@outlook.com>
2024-07-14 00:50:06 +0900

    refactor: import.

--- turu-mysql/tests/turu/test_mysql_mock.py
+++ turu-mysql/tests/turu/test_mysql_mock.py
@@ -1,10 +1,10 @@
 from dataclasses import dataclass
 
 import pytest
-import turu.mysql
 from pydantic import BaseModel
 from turu.core import tag
 from turu.core.mock.exception import TuruMockResponseTypeMismatchError
+from turu.mysql import MockConnection
 
 
 class Row(BaseModel):
@@ -12,26 +12,26 @@ class Row(BaseModel):
 
 
 class TestTuruMysqlMock:
-    def test_execute(self, mock_connection: turu.mysql.MockConnection):
+    def test_execute(self, mock_connection: MockConnection):
         expected = [(1,)]
         mock_connection.inject_response(None, expected)
 
         assert mock_connection.execute("select 1").fetchall() == expected
 
-    def test_execute_fetchone(self, mock_connection: turu.mysql.MockConnection):
+    def test_execute_fetchone(self, mock_connection: MockConnection):
         expected = [(1,)]
         mock_connection.inject_response(None, expected)
 
         assert mock_connection.execute("select 1").fetchone() == expected[0]
 
-    def test_execute_map_fetchone(self, mock_connection: turu.mysql.MockConnection):
+    def test_execute_map_fetchone(self, mock_connection: MockConnection):
         expected = [Row(id=1)]
         mock_connection.inject_response(Row, expected)
 
         with mock_connection.execute_map(Row, "select 1") as cursor:
             assert cursor.fetchone() == expected[0]
 
-    def test_execute_map_fetchmany(self, mock_connection: turu.mysql.MockConnection):
+    def test_execute_map_fetchmany(self, mock_connection: MockConnection):
         expected = [Row(id=1), Row(id=2)]
         mock_connection.inject_response(Row, expected)
 
@@ -40,9 +40,7 @@ class TestTuruMysqlMock:
             assert cursor.fetchone() == Row(id=2)
             assert cursor.fetchone() is None
 
-    def test_execute_map_fetchmany_with_size(
-        self, mock_connection: turu.mysql.MockConnection
-    ):
+    def test_execute_map_fetchmany_with_size(self, mock_connection: MockConnection):
         mock_connection.inject_response(Row, [Row(id=1), Row(id=2), Row(id=3)])
 
         with mock_connection.cursor().execute_map(
@@ -51,7 +49,7 @@ class TestTuruMysqlMock:
             assert cursor.fetchmany(2) == [Row(id=1), Row(id=2)]
             assert cursor.fetchmany(2) == [Row(id=3)]
 
-    def test_execute_map_fetchall(self, mock_connection: turu.mysql.MockConnection):
+    def test_execute_map_fetchall(self, mock_connection: MockConnection):
         expected = [Row(id=1), Row(id=2)]
 
         mock_connection.inject_response(Row, expected)
@@ -63,14 +61,14 @@ class TestTuruMysqlMock:
         assert cursor.fetchall() == expected
         assert cursor.fetchone() is None
 
-    def test_executemany(self, mock_connection: turu.mysql.MockConnection):
+    def test_executemany(self, mock_connection: MockConnection):
         expected = [(1,)]
         mock_connection.inject_response(None, expected)
 
         with mock_connection.cursor().executemany("select 1", []) as cursor:
             assert cursor.fetchall() == expected
 
-    def test_executemany_map(self, mock_connection: turu.mysql.MockConnection):
+    def test_executemany_map(self, mock_connection: MockConnection):
         expected = [Row(id=1), Row(id=2)]
         mock_connection.inject_response(Row, expected)
 
@@ -79,14 +77,14 @@ class TestTuruMysqlMock:
         ) as cursor:
             assert cursor.fetchall() == expected
 
-    def test_execute_iter(self, mock_connection: turu.mysql.MockConnection):
+    def test_execute_iter(self, mock_connection: MockConnection):
         expected = [(1,), (2,)]
         mock_connection.inject_response(None, expected)
 
         with mock_connection.cursor().execute("select 1 union all select 2") as cursor:
             assert list(cursor) == expected
 
-    def test_execute_map_iter(self, mock_connection: turu.mysql.MockConnection):
+    def test_execute_map_iter(self, mock_connection: MockConnection):
         expected = [Row(id=1), Row(id=2)]
         mock_connection.inject_response(Row, expected)
         with mock_connection.cursor().execute_map(
@@ -94,29 +92,29 @@ class TestTuruMysqlMock:
         ) as cursor:
             assert list(cursor) == expected
 
-    def test_mock_connection_close(self, mock_connection: turu.mysql.MockConnection):
+    def test_mock_connection_close(self, mock_connection: MockConnection):
         mock_connection.close()
 
-    def test_mock_connection_commit(self, mock_connection: turu.mysql.MockConnection):
+    def test_mock_connection_commit(self, mock_connection: MockConnection):
         mock_connection.commit()
 
-    def test_mock_connection_rollback(self, mock_connection: turu.mysql.MockConnection):
+    def test_mock_connection_rollback(self, mock_connection: MockConnection):
         mock_connection.rollback()
 
-    def test_cursor_rowcount(self, mock_connection: turu.mysql.MockConnection):
+    def test_cursor_rowcount(self, mock_connection: MockConnection):
         cursor = mock_connection.cursor()
         assert cursor.rowcount == -1
 
-    def test_cursor_arraysize(self, mock_connection: turu.mysql.MockConnection):
+    def test_cursor_arraysize(self, mock_connection: MockConnection):
         cursor = mock_connection.cursor()
         assert cursor.arraysize == 1
 
-    def test_cursor_arraysize_setter(self, mock_connection: turu.mysql.MockConnection):
+    def test_cursor_arraysize_setter(self, mock_connection: MockConnection):
         cursor = mock_connection.cursor()
         cursor.arraysize = 2
         assert cursor.arraysize == 2
 
-    def test_execute_with_tag(self, mock_connection: turu.mysql.MockConnection):
+    def test_execute_with_tag(self, mock_connection: MockConnection):
         @dataclass
         class Table:
             pass
@@ -129,9 +127,7 @@ class TestTuruMysqlMock:
                 is None
             )
 
-    def test_execute_with_tag_when_other_table(
-        self, mock_connection: turu.mysql.MockConnection
-    ):
+    def test_execute_with_tag_when_other_table(self, mock_connection: MockConnection):
         @dataclass
         class Table:
             pass
@@ -147,7 +143,7 @@ class TestTuruMysqlMock:
                 cursor.execute_with_tag(tag.Insert[OtherTable], "INSERT table")
 
     def test_execute_with_tag_when_other_operation(
-        self, mock_connection: turu.mysql.MockConnection
+        self, mock_connection: MockConnection
     ):
         @dataclass
         class Table:
@@ -159,7 +155,7 @@ class TestTuruMysqlMock:
             with mock_connection.cursor() as cursor:
                 cursor.execute_with_tag(tag.Update[Table], "UPDATE table")
 
-    def test_executemany_with_tag(self, mock_connection: turu.mysql.MockConnection):
+    def test_executemany_with_tag(self, mock_connection: MockConnection):
         @dataclass
         class Table:
             pass
@@ -175,7 +171,7 @@ class TestTuruMysqlMock:
             )
 
     def test_executemany_with_tag_when_other_table(
-        self, mock_connection: turu.mysql.MockConnection
+        self, mock_connection: MockConnection
     ):
         @dataclass
         class Table:
@@ -192,7 +188,7 @@ class TestTuruMysqlMock:
                 cursor.executemany_with_tag(tag.Insert[OtherTable], "INSERT table", [])
 
     def test_executemany_with_tag_when_other_operation(
-        self, mock_connection: turu.mysql.MockConnection
+        self, mock_connection: MockConnection
     ):
         @dataclass
         class Table:
--- turu-mysql/tests/turu/test_mysql_mock_async.py
+++ turu-mysql/tests/turu/test_mysql_mock_async.py
@@ -1,10 +1,10 @@
 from dataclasses import dataclass
 
 import pytest
-import turu.mysql
 from pydantic import BaseModel
 from turu.core import tag
 from turu.core.mock.exception import TuruMockResponseTypeMismatchError
+from turu.mysql import MockAsyncConnection
 
 
 class Row(BaseModel):
@@ -13,7 +13,7 @@ class Row(BaseModel):
 
 class TestTuruMysqlMockAsync:
     @pytest.mark.asyncio
-    async def test_execute(self, mock_async_connection: turu.mysql.MockAsyncConnection):
+    async def test_execute(self, mock_async_connection: MockAsyncConnection):
         expected = [(1,)]
         mock_async_connection.inject_response(None, expected)
 
@@ -21,9 +21,7 @@ class TestTuruMysqlMockAsync:
             assert await cursor.fetchall() == expected
 
     @pytest.mark.asyncio
-    async def test_execute_fetchone(
-        self, mock_async_connection: turu.mysql.MockAsyncConnection
-    ):
+    async def test_execute_fetchone(self, mock_async_connection: MockAsyncConnection):
         expected = [(1,)]
         mock_async_connection.inject_response(None, expected)
 
@@ -32,7 +30,7 @@ class TestTuruMysqlMockAsync:
 
     @pytest.mark.asyncio
     async def test_execute_map_fetchone(
-        self, mock_async_connection: turu.mysql.MockAsyncConnection
+        self, mock_async_connection: MockAsyncConnection
     ):
         expected = [Row(id=1)]
         mock_async_connection.inject_response(Row, expected)
@@ -42,7 +40,7 @@ class TestTuruMysqlMockAsync:
 
     @pytest.mark.asyncio
     async def test_execute_map_fetchmany(
-        self, mock_async_connection: turu.mysql.MockAsyncConnection
+        self, mock_async_connection: MockAsyncConnection
     ):
         expected = [Row(id=1), Row(id=2)]
         mock_async_connection.inject_response(Row, expected)
@@ -56,7 +54,7 @@ class TestTuruMysqlMockAsync:
 
     @pytest.mark.asyncio
     async def test_execute_map_fetchmany_with_size(
-        self, mock_async_connection: turu.mysql.MockAsyncConnection
+        self, mock_async_connection: MockAsyncConnection
     ):
         mock_async_connection.inject_response(Row, [Row(id=1), Row(id=2), Row(id=3)])
 
@@ -68,7 +66,7 @@ class TestTuruMysqlMockAsync:
 
     @pytest.mark.asyncio
     async def test_execute_map_fetchall(
-        self, mock_async_connection: turu.mysql.MockAsyncConnection
+        self, mock_async_connection: MockAsyncConnection
     ):
         expected = [Row(id=1), Row(id=2)]
 
@@ -81,9 +79,7 @@ class TestTuruMysqlMockAsync:
             assert await cursor.fetchone() is None
 
     @pytest.mark.asyncio
-    async def test_executemany(
-        self, mock_async_connection: turu.mysql.MockAsyncConnection
-    ):
+    async def test_executemany(self, mock_async_connection: MockAsyncConnection):
         expected = [(1,)]
         mock_async_connection.inject_response(None, expected)
 
@@ -91,9 +87,7 @@ class TestTuruMysqlMockAsync:
             assert await cursor.fetchall() == expected
 
     @pytest.mark.asyncio
-    async def test_executemany_map(
-        self, mock_async_connection: turu.mysql.MockAsyncConnection
-    ):
+    async def test_executemany_map(self, mock_async_connection: MockAsyncConnection):
         expected = [Row(id=1), Row(id=2)]
         mock_async_connection.inject_response(Row, expected)
 
@@ -103,9 +97,7 @@ class TestTuruMysqlMockAsync:
             assert await cursor.fetchall() == expected
 
     @pytest.mark.asyncio
-    async def test_execute_iter(
-        self, mock_async_connection: turu.mysql.MockAsyncConnection
-    ):
+    async def test_execute_iter(self, mock_async_connection: MockAsyncConnection):
         expected = [(1,), (2,)]
         mock_async_connection.inject_response(None, expected)
 
@@ -115,9 +107,7 @@ class TestTuruMysqlMockAsync:
             assert [row async for row in cursor] == expected
 
     @pytest.mark.asyncio
-    async def test_execute_map_iter(
-        self, mock_async_connection: turu.mysql.MockAsyncConnection
-    ):
+    async def test_execute_map_iter(self, mock_async_connection: MockAsyncConnection):
         expected = [Row(id=1), Row(id=2)]
         mock_async_connection.inject_response(Row, expected)
         async with await mock_async_connection.execute_map(
@@ -127,48 +117,42 @@ class TestTuruMysqlMockAsync:
 
     @pytest.mark.asyncio
     async def test_mock_connection_close(
-        self, mock_async_connection: turu.mysql.MockAsyncConnection
+        self, mock_async_connection: MockAsyncConnection
     ):
         await mock_async_connection.close()
 
     @pytest.mark.asyncio
     async def test_mock_connection_commit(
-        self, mock_async_connection: turu.mysql.MockAsyncConnection
+        self, mock_async_connection: MockAsyncConnection
     ):
         await mock_async_connection.commit()
 
     @pytest.mark.asyncio
     async def test_mock_connection_rollback(
-        self, mock_async_connection: turu.mysql.MockAsyncConnection
+        self, mock_async_connection: MockAsyncConnection
     ):
         await mock_async_connection.rollback()
 
     @pytest.mark.asyncio
-    async def test_cursor_rowcount(
-        self, mock_async_connection: turu.mysql.MockAsyncConnection
-    ):
+    async def test_cursor_rowcount(self, mock_async_connection: MockAsyncConnection):
         cursor = await mock_async_connection.cursor()
         assert cursor.rowcount == -1
 
     @pytest.mark.asyncio
-    async def test_cursor_arraysize(
-        self, mock_async_connection: turu.mysql.MockAsyncConnection
-    ):
+    async def test_cursor_arraysize(self, mock_async_connection: MockAsyncConnection):
         cursor = await mock_async_connection.cursor()
         assert cursor.arraysize == 1
 
     @pytest.mark.asyncio
     async def test_cursor_arraysize_setter(
-        self, mock_async_connection: turu.mysql.MockAsyncConnection
+        self, mock_async_connection: MockAsyncConnection
     ):
         cursor = await mock_async_connection.cursor()
         cursor.arraysize = 2
         assert cursor.arraysize == 2
 
     @pytest.mark.asyncio
-    async def test_execute_with_tag(
-        self, mock_async_connection: turu.mysql.MockAsyncConnection
-    ):
+    async def test_execute_with_tag(self, mock_async_connection: MockAsyncConnection):
         @dataclass
         class Table:
             pass
@@ -184,7 +168,7 @@ class TestTuruMysqlMockAsync:
 
     @pytest.mark.asyncio
     async def test_execute_with_tag_when_other_table(
-        self, mock_async_connection: turu.mysql.MockAsyncConnection
+        self, mock_async_connection: MockAsyncConnection
     ):
         @dataclass
         class Table:
@@ -202,7 +186,7 @@ class TestTuruMysqlMockAsync:
 
     @pytest.mark.asyncio
     async def test_execute_with_tag_when_other_operation(
-        self, mock_async_connection: turu.mysql.MockAsyncConnection
+        self, mock_async_connection: MockAsyncConnection
     ):
         @dataclass
         class Table:
@@ -216,7 +200,7 @@ class TestTuruMysqlMockAsync:
 
     @pytest.mark.asyncio
     async def test_executemany_with_tag(
-        self, mock_async_connection: turu.mysql.MockAsyncConnection
+        self, mock_async_connection: MockAsyncConnection
     ):
         @dataclass
         class Table:
@@ -235,7 +219,7 @@ class TestTuruMysqlMockAsync:
 
     @pytest.mark.asyncio
     async def test_executemany_with_tag_when_other_table(
-        self, mock_async_connection: turu.mysql.MockAsyncConnection
+        self, mock_async_connection: MockAsyncConnection
     ):
         @dataclass
         class Table:
@@ -255,7 +239,7 @@ class TestTuruMysqlMockAsync:
 
     @pytest.mark.asyncio
     async def test_executemany_with_tag_when_other_operation(
-        self, mock_async_connection: turu.mysql.MockAsyncConnection
+        self, mock_async_connection: MockAsyncConnection
     ):
         @dataclass
         class Table:
